b8f897db5d4004e4aec54926cc1dc61fd7c1af89
kadoppe <kadoppe@me.com>
2014-11-09 22:09:38 +0900

    Create script src hash page

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var morgan = require('morgan')
 var app = express();
+
+app.use(morgan());
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
   res.render('index', { title: "kadoppe's sandbox" });
+});
+
+app.get('/script-src-hash', function(req, res) {
+  res.render('script-src-hash', { title: "script src hash problem "});
 });
 
 var server = app.listen(3000);
